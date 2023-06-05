2332c4a645c05755052fc255434ac142100dceaf
John Muchovej <5000729+jmuchovej@users.noreply.github.com>
2023-06-05 13:12:55 -0400

    Remove file I/O from tailwind export JS

--- scripts/exportTailwindColors.cjs
+++ scripts/exportTailwindColors.cjs
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 const _ = require("lodash");
 const colorPalette = require("tailwindcss/colors");
-const path = require("path");
-const fs = require("fs");
 
 // The relevant color scales are `object`s, but Tailwind supports the previous names
 //   for some colors so they need to be removed, as well. (String colors are
@@ -26,17 +24,8 @@ const colors = _.reduce(desiredColors, (colorMap, colorScale2Hex, colorName) => 
 // This is fluff just to make the output file a little more readable
 const longestColorNameScale = _.size(_.maxBy(_.keys(colors), _.size));
 
-// Converting `colors` into an array of strings, then a single string to be written
-const tailwindTXT = _.join(_.reduce(colors, (colorArr, colorHex, colorName) => {
-  colorArr.push(`${_.padEnd(colorName, longestColorNameScale, " ")}    ${colorHex}`);
-  return colorArr;
-}, []), "\n");
-
-// `__dirname` is a constant that points to the directory where this script is located
-const datapath = path.join(__dirname, "../data/tailwind.txt");
-
-// Write-out the TailwindCSS colors to `data/tailwind.txt`
-fs.writeFile(datapath, tailwindTXT, { encoding: "utf-8", flag: "w", }, (err) => {
-  if (err) throw err;
-  console.log(`Successfully wrote Tailwind colors to \`${datapath}\`!`);
-});
+// Printing each [color, hex] pair to stdout so the Julia script can write to
+//   `data/tailwind.txt`.
+for (const [colorName, colorHex] of Object.entries(colors)) {
+  console.log(`${_.padEnd(colorName, longestColorNameScale, " ")}    ${colorHex}`)
+}
